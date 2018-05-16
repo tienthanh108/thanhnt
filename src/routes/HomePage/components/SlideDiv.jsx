d2d9d27a804a4c76b64ebb7e3ba140c1927ee7bf
VPBANK\thanhnt43 <thanhnt43@vpbank.com.vn>
2018-05-16 09:23:29 +0700

    Fix bug

--- src/routes/HomePage/components/SlideDiv.jsx
+++ src/routes/HomePage/components/SlideDiv.jsx
@@ -3,8 +3,25 @@ import Slider from 'react-slick'
 // import slider1 from 'assets/slide1.jpg'
 // import slider2 from 'assets/slide2.jpg'
 // import slider3 from 'assets/slide3.jpg'
+import slider1 from 'assets/next-btn.png'
+import slider2 from 'assets/prev-btn.png'
 import classes from './SlideDiv.scss'
 
+function SampleNextArrow(props) {
+  return (
+    <div {...props}>
+      <img alt='next' src={slider1} />
+    </div>
+  )
+}
+
+function SamplePrevArrow(props) {
+  return (
+    <div {...props}>
+      <img alt='prev' src={slider2} />
+    </div>
+  )
+}
 const SliderDiv = (props) => {
   const settings = {
     dots: false,
@@ -12,6 +29,8 @@ const SliderDiv = (props) => {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
     responsive: [{
       breakpoint: 768,
       settings: {
@@ -26,11 +45,11 @@ const SliderDiv = (props) => {
       {
         props.elements && props.elements.length > 0 &&
 
-        <Slider {...settings}>
+        <Slider {...settings} id='sliderProduct'>
           {
               props.elements.map((item, index) =>
                 <div data-index={index} key={index}>
-                  <img src={item.imagePath} alt={item.description} />
+                  <img src={item.imagePath} className={classes.img} alt={item.description} />
                   <div className={classes.title} onClick={() => props.navigateProject(item.id)}>{item.name}</div>
                 </div>,
               )
